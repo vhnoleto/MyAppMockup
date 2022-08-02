b5aa7c821473f10c7ecef70bbe29863624aeb16e
unknown <victornoleto43@gmail.com>
2022-08-01 22:17:06 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myappmockup/Start_activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myappmockup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Start_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_layout);
+    }
+}
